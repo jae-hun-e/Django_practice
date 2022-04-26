3321927ad18fec04dd3a9409ed4b33279421fe19
조재훈 <jaehun@jojaehun-ui-MacBookPro.local>
2022-04-27 08:30:51 +0900

    변수 넘기기

--- costaurant/foods/views.py
+++ costaurant/foods/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from datetime import datetime
 
 
 def index(request):
-    return render(request, 'foods/index.html')
+    today = datetime.today().date()
+    context = {"date": today}
+    return render(request, 'foods/index.html', context=context)
+
